66255eb5df6592da2665d4d8319bef95041c9517
Helen Wright <hwright2@ed.ac.uk>
2023-11-21 14:31:36 +0000

    empty framework for SA options

---
+++ R/run_SA.R
@@ -0,0 +1,13 @@
+#### Sensitivity Analysis ####
+run_SA <- function(matched_object, .data, SA_type){ # sensitivity analysis type
+  switch(SA_type,
+         rosenbaum_SA = {
+           
+         },
+         PS_calibration = {
+           
+         },
+         VW_A_formula = {
+           
+         })
+}
