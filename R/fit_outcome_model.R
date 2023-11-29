f81931ad160dd9b708ba09514d1550249980e408
Helen Wright <hwright2@ed.ac.uk>
2023-11-29 11:56:20 +0000

    correct MI ME formula to include t*cov

--- R/fit_outcome_model.R
+++ R/fit_outcome_model.R
@@ -140,16 +140,17 @@ outcome_marginal_effects <- function(balanced_data,
                              matching_variable, covariates, 
                              missing_method,...){
   if(!is.null(covariates)){
-    model_formula = as.formula(paste0(outcome_variable,"~",treatment_variable,"*(",paste0(matching_variable, covariates, collapse="+"), ")"))
+    model_formula <- as.formula(paste0(outcome_variable, " ~ ", treatment_variable, " * (", matching_variable, " + ", paste(covariates, collapse = " + "), ")"))
   } else{
     model_formula = as.formula(paste0(outcome_variable,"~",treatment_variable,"*(",paste0(matching_variable, collapse="+"), ")"))
   }
-  
+  #print(model_formula)
   if(extracted_balanced_data$process == "mi_psm"){
     lm_model_fit <- lapply(complete(balanced_data, "all"), function(d) {
       lm(model_formula, data = d,
          weights = weights)
     })
+    print(lm_model_fit)
     model_fit <- lapply(lm_model_fit, function(fit) {
       marginaleffects::avg_comparisons(fit, newdata = subset(fit$data, treatment_variable == 1),
                                        variables = treatment_variable, wts = "weights", vcov = ~subclass)
