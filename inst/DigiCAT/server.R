9affea9309949941d75402eb08ca246d67422833
Hannah Casey <53822789+hannahcasey@users.noreply.github.com>
2024-06-03 11:29:18 +0100

    Update server.R
    Allow tool to be run without run_DigiCAT() - will set file path as home directory automatically

--- inst/DigiCAT/server.R
+++ inst/DigiCAT/server.R
@@ -7,6 +7,11 @@ i18n <- Translator$new(translation_csvs_path = system.file("DigiCAT/translation/
 ## Increase maximum upload file size
 options(shiny.maxRequestSize = 50*1024^2)
 
+## If app not run using "run_DigiCAT", set home directory as default directory
+if(!exists("file_path_global")){
+  file_path_global <- fs::path_home()
+}
+
 server <- function(input, output, session) {
   
   
