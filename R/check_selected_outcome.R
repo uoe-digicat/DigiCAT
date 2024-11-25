0255538dffc4fc07eb9540bc70c35a58f4510573
Hannah Casey <53822789+hannahcasey@users.noreply.github.com>
2024-11-25 18:49:29 +0000

    Reapply "Merge pull request #173 from uoe-digicat/fix_outcome_var_type"
    This reverts commit 05294d15e67493798c8968e5c7c6921cc462a212.

--- R/check_selected_outcome.R
+++ R/check_selected_outcome.R
@@ -1,16 +1,18 @@
 
-check_selected_outcome <- function(data, outcome){
+check_selected_outcome <- function(data, outcome, categorical_variables){
   
-  non_na_values <- length(setdiff(data[[outcome]], c(NA)))
-  
-  if (non_na_values == 2){
-    var_type = 'Binary'
-  } else if (non_na_values > 2 & non_na_values <= 5){
-    var_type = 'Categorical'
-  } else if (non_na_values > 5){
+  ## If no categorical variables, outcome is continuous
+  if(is.null(categorical_variables)){
     var_type = 'Continuous'
   } else{
-    var_type = 'Constant'
+    non_na_values <- length(setdiff(data[[outcome]], c(NA)))
+    if (non_na_values == 2){
+      var_type = 'Binary'
+    } else if (outcome %in% categorical_variables){
+      var_type = 'Categorical'
+    } else if (!outcome %in% categorical_variables){
+      var_type = 'Continuous'
+    }
   }
   return(var_type)
 }
