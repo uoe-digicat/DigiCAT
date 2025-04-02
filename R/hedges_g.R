0f0828860f2524cd934b6c4c5f6024b01458ecba
Hannah Casey <53822789+hannahcasey@users.noreply.github.com>
2025-04-02 10:54:31 +0100

    Update hedges_g.R

--- R/hedges_g.R
+++ R/hedges_g.R
@@ -4,7 +4,7 @@
 #' @param outcome_variable Name of outcome variable
 #' @param missing_method 
 #' @param balanced_data Balanced data object
-#' @param outcome_results Outcome model object 
+#' @param outcome_model Outcome model object 
 #' @param weighting_variable Character string matching column name of your weighting variable
 #' @param cluster_variable Character string matching column name of your clustering variable
 #' @param strata_variable Character string matching column name of your stratification variable
@@ -19,7 +19,7 @@ hedges_g <- function(treatment_variable,
                      missing_method, 
                      outcome_variable, 
                      balanced_data, 
-                     outcome_results,  
+                     outcome_model,  
                      weighting_variable = NULL, 
                      cluster_variable = NULL, 
                      strata_variable = NULL){
@@ -49,7 +49,7 @@ hedges_g <- function(treatment_variable,
       ids = cluster_formula,
       strata = strata_formula,
       weights = weighting_formula,
-      data = outcome_results$extracted_balanced_data[[1]]
+      data = outcome_model$extracted_balanced_data[[1]]
     )
     
     # ## Get mean difference
@@ -61,17 +61,17 @@ hedges_g <- function(treatment_variable,
     # mean_diff <- coef(svy_mean_diff)[2]
     
     ## Will use coefficient already calculated? Same as above if "unadjusted" outcome model used
-    mean_diff <- outcome_results$standardised_format$`Coefficient Estimate`
+    mean_diff <- outcome_model$standardised_format$`Coefficient Estimate`
     
     # Get SD of outcome in treatment group
     treatment_data <- subset(svy_design, get(treatment_variable) == 1)
     f <- paste0(" ~ as.numeric(as.character(", outcome_variable, "))")
-    SD_t <- sqrt(svyvar(as.formula(f), design = treatment_data)[1])
+    var_t <- svyvar(as.formula(f), design = treatment_data)[1]
     
     # Get SD of outcome in control group
     control_data <- subset(svy_design, get(treatment_variable) == 0)
     f <- paste0(" ~ as.numeric(as.character(", outcome_variable, "))")
-    SD_c <- sqrt(svyvar(as.formula(f), design = control_data)[1])
+    var_c <- svyvar(as.formula(f), design = control_data)[1]
     
     # Get N treat and N control (effective sample sizes)
     f <- paste0(" ~ ", treatment_variable)
@@ -79,27 +79,27 @@ hedges_g <- function(treatment_variable,
     N_c <- sum(svytotal(as.formula(f), design = control_data))
     
     # Hedge's g calculation
-    pooled_variance <- (((SD_t^2) * (N_t - 1)) + ((SD_c^2) * (N_c - 1))) / (N_t + N_c - 2)
+    pooled_variance <- (((var_t) * (N_t - 1)) + ((var_c) * (N_c - 1))) / (N_t + N_c - 2)
     g <- mean_diff / sqrt(pooled_variance)
   }
 
   if(missing_method == "weighting"){
     
     # Survey design already defined
-    svy_design <- outcome_results$extracted_balanced_data[[1]]
+    svy_design <- outcome_model$extracted_balanced_data[[1]]
     
     ## Coefficient already defined
-    mean_diff <- outcome_results$standardised_format$`Coefficient Estimate`
+    mean_diff <- outcome_model$standardised_format$`Coefficient Estimate`
     
     # Get SD of outcome in treatment group
     treatment_data <- subset(svy_design, get(treatment_variable) == 1)
     f <- paste0(" ~ ", outcome_variable)
-    SD_t <- sqrt(svyvar(as.formula(f), design = treatment_data, na.rm=TRUE)[1])
+    var_t <- svyvar(as.formula(f), design = treatment_data, na.rm=TRUE)[1]
     
     # Get SD of outcome in control group
     control_data <- subset(svy_design, get(treatment_variable) == 0)
     f <- paste0(" ~ ", outcome_variable)
-    SD_c <- sqrt(svyvar(as.formula(f), design = control_data,  na.rm=TRUE)[1])
+    var_c <- svyvar(as.formula(f), design = control_data, na.rm=TRUE)[1]
     
     # Get N treat and N control (effective sample sizes)
     f <- paste0(" ~ ", treatment_variable)
@@ -107,14 +107,14 @@ hedges_g <- function(treatment_variable,
     N_c <- sum(svytotal(as.formula(f), design = control_data))
     
     # Hedge's g calculation
-    pooled_variance <- (((SD_t^2) * (N_t - 1)) + ((SD_c^2) * (N_c - 1))) / (N_t + N_c - 2)
+    pooled_variance <- (((var_t) * (N_t - 1)) + ((var_c) * (N_c - 1))) / (N_t + N_c - 2)
     g <- mean_diff / sqrt(pooled_variance)
   }
   
   if (missing_method == "mi") {
     
     ## Get imputed dataset
-    imputed_data <- as.list(outcome_results$extracted_balanced_data[[1]])
+    imputed_data <- as.list(outcome_model$extracted_balanced_data[[1]])
     
     # Define survey design
     if (!is.null(cluster_variable)) {
@@ -147,7 +147,7 @@ hedges_g <- function(treatment_variable,
     })
     
     ## Get mean difference
-    mean_diff <- outcome_results$standardised_format$`Coefficient Estimate`
+    mean_diff <- outcome_model$standardised_format$`Coefficient Estimate`
     
     ## Get SD of outcome in treatment group
     results <- lapply(svy_design_list, function(svy_design) {
@@ -161,21 +161,21 @@ hedges_g <- function(treatment_variable,
       
       # Calculate statistics
       list(
-        SD_t = sqrt(svyvar(f_SD, design = treatment_data)[1]),  # Standard deviation for treatment group
-        SD_c = sqrt(svyvar(f_SD, design = control_data)[1]),    # Standard deviation for control group
+        var_t = svyvar(f_SD, design = treatment_data)[1],  # Variance of treatment group
+        var_c = svyvar(f_SD, design = control_data)[1],    # Variance of control group
         N_t = svytotal(f_N, design = treatment_data),     # Total for treatment group
         N_c = svytotal(f_N, design = control_data)        # Total for control group
       )
     })
     
     ## Aggregate results across imputations
-    mean_SD_t <- mean(sapply(results, `[[`, "SD_t"))
-    mean_SD_c <- mean(sapply(results, `[[`, "SD_c"))
-    mean_N_t <- mean(sapply(results, `[[`, "N_t"))*2
-    mean_N_c <- mean(sapply(results, `[[`, "N_c"))*2
+    mean_var_t <- mean(sapply(results, `[[`, "var_t"))
+    mean_var_c <- mean(sapply(results, `[[`, "var_c"))
+    mean_N_t <- mean(sapply(results, `[[`, "N_t"))
+    mean_N_c <- mean(sapply(results, `[[`, "N_c"))
 
     ## Pooled standard deviation
-    pooled_SD <- sqrt(((mean_SD_t^2) * (mean_N_t - 1) + (mean_SD_c^2) * (mean_N_c - 1)) / (mean_N_t + mean_N_c - 2))
+    pooled_SD <- sqrt((mean_var_t) * (mean_N_t - 1) + (mean_var_c) * (mean_N_c - 1) / (mean_N_t + mean_N_c - 2))
     
     ## Hedge's g calculation
     g <- mean_diff / pooled_SD
